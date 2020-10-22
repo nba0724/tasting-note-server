e2b66db63ac63d055cf6f1e551a67909adfd59fd
nba0724 <eandidiary@gmail.com>
2020-10-22 12:02:23 +0900

    helmet and morgan install and set

--- index.js
+++ index.js
@@ -1,11 +1,32 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const port = process.env.PORT || 8000;
+const app = express();
+const cluster = require("cluster");
+const os = require("os");
+const helmet = require("helmet");
+const logger = require("morgan");
+app.use(logger("combined"));
+const numCPUs = os.cpus().length;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    console.info(" worker %dを起動します。", i);
+    cluster.fork();
+  }
+  cluster.on("exit", (worker, code, signal) => {
+    console.error(
+      " worker %d が停止しました。 (%s).",
+      worker.process.pid,
+      signal || code
+    );
+    cluster.fork();
+  });
+} else {
+  // Middlewareの設定
+  app.use(helmet());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  app.listen(port);
+  console.info("Server is UP");
+}
